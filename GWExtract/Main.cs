df07334f902a21405cae66cc0b93ac2a309c6953
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2012-12-11 21:27:04 +0100

    Initial import

---
+++ GWExtract/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GWExtract
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
